fdae9276947cabd3b5a5816023835eadb90d96cd
Sivadej K <bombyk@gmail.com>
2020-03-06 20:19:44 -0900

    posts

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template, redirect, flash, session, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SECRET_KEY']='YOUR_SECRET_KEY'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -42,15 +42,16 @@ def create_user():
 @app.route('/users/<int:id>')
 def show_user_details(id):
     user = User.query.get_or_404(id)
-    return render_template('user_details.html', user=user)
+    posts = Post.query.filter_by(author_id=id)
+    return render_template('user_details.html', user=user, posts=posts)
 
 @app.route('/users/<int:id>/edit')
-def show_edit_form(id):
+def show_edit_user_form(id):
     user = User.query.get_or_404(id)
     return render_template('edit_user.html', user=user)
 
 @app.route('/users/<int:id>/edit', methods=['POST'])
-def do_edit(id):
+def do_edit_user(id):
     user = User.query.get_or_404(id)
     user.first_name = request.form['first_name']
     user.last_name = request.form['last_name']
@@ -65,3 +66,45 @@ def remove_user(id):
     db.session.delete(user_to_delete)
     db.session.commit()
     return redirect(url_for('show_users'))
+
+@app.route('/users/<int:id>/posts/new')
+def show_add_post_form(id):
+    user = User.query.get_or_404(id)
+    return render_template('create_new_post.html', user=user)
+
+@app.route('/users/<int:id>/posts/new', methods=['POST'])
+def create_new_post(id):
+    new_post = Post(
+        title = request.form['title'],
+        content = request.form['content'],
+        author_id = id,
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f'/users/{id}')
+
+@app.route('/posts/<int:id>')
+def show_post_detail(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/<int:id>/delete', methods=['POST'])
+def remove_post(id):
+    post_to_delete = Post.query.get_or_404(id)
+    return_route = f'/users/{post_to_delete.user.id}'
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(return_route)
+
+@app.route('/posts/<int:id>/edit')
+def show_edit_post_form(id):
+    post = Post.query.get_or_404(id)
+    return render_template('edit_post.html', post=post)
+
+@app.route('/posts/<int:id>/edit', methods=['POST'])
+def do_edit_post(id):
+    post = Post.query.get_or_404(id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.commit()
+    return redirect(f'/posts/{id}')
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -16,9 +17,19 @@ class User(db.Model):
     last_name = db.Column(db.String(30), nullable=False)
     img_url = db.Column(db.String(50), nullable=True)
 
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     def get_full_name(self):
         return(f'{self.first_name} {self.last_name}')
     
 #    @classmethod
 #    def get_users_by_last_name(cls):
 #        return cls.query.order_by('last_name').all()
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
